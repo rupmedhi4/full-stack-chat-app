cfe4a78870cb65aa61568de9f72e91839a7f8537
Rup medhi <rupmmedhi@gmail.com>
2024-09-08 22:41:39 +0530

    avatar

--- frontend/src/components/home/RightPart/ChatUser.js
+++ frontend/src/components/home/RightPart/ChatUser.js
@@ -7,13 +7,20 @@ export default function ChatUser() {
     const { socket, onlineUsers } = useSocketContext()
     const isOnline = onlineUsers.includes(selectedConversation._id)
 
+    let avatar = selectedConversation.fullname.split(" ");
+
+    let firstNameLetter = avatar[0][0] || '';
+    let lastNameLetter = avatar.length > 1 ? (avatar[avatar.length - 1][0] || '') : (avatar[0][1] || '');
+
+    avatar = firstNameLetter.toUpperCase() + lastNameLetter.toUpperCase();
+
     return (
         <div className=' h-[10vh] flex space-x-3 items-center justify-center bg-gray-800 hover:bg-gray-700 duration-300'>
-            <div className={`avatar ${isOnline ? "online" : ""}`}>
-                <div className="w-16 rounded-full">
-                  <img src="/Dp.jpg" alt=" img" srcset="" />
+             <div className="w-12 h-12 rounded-full bg-white ">
+                    <span className='text-black font-bold flex  justify-center mt-3'>
+                        {avatar}
+                    </span>
                 </div>
-            </div>
             <div >
                 <h1 className='text-xl'>{selectedConversation ? selectedConversation.fullname : null}</h1>
                 <span className='text-sm'>{isOnline ? "Online" : "Offline"}</span>
